9b4ef26f1678535b18fe1bd4c6893fae98b6b65a
Michael Pope <popemray1986@gmail.com>
2018-03-04 01:08:13 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -74,5 +74,17 @@ class Dog
     Dog.new(name:name, breed:breed, id:id)
   end
 
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first    
+  end
+
 
 end
